56a44d391549825c44616ba2e6b897167ec0f4a2
Xiong-Alex <102581551+Xiong-Alex@users.noreply.github.com>
2025-12-30 11:46:30 -0500

    Time: 0 ms (100.00%) | Memory: 43 MB (50.16%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+
+        // start from the back
+        int i = s.length() - 1;
+        int length = 0;
+
+        // skip all empty ' '
+        while(i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        // count all valid char until ' '
+        while(i >= 0 && s.charAt(i) != ' ') {
+            i--;
+            length++;
+        }
+
+        return length;
+    }
+}
+
+// or do a single pass and count l
+
+// String[] words = s.split("\\s");
+// return words[words.length - 1].length();
