4d582c26112d94cd7040b8e09e702c556e997d53
Xiong-Alex <102581551+Xiong-Alex@users.noreply.github.com>
2026-01-08 03:08:39 -0500

    Time: 1142 ms (11.51%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,21 +1,19 @@
 # Write your MySQL query statement below
 
-select s.student_id, s.student_name, sbj.subject_name, 
-count(e.subject_name) as attended_exams
+select s.student_id, s.student_name, sub.subject_name, Count(e.subject_name) as attended_exams
 from students s
-cross join subjects sbj
-left join Examinations e 
-    on s.student_id = e.student_id 
-    and e.subject_name = sbj.subject_name
 
-group by   
+cross join subjects sub
+left join examinations e 
+
+on s.student_id = e.student_id
+and sub.subject_name = e.subject_name
+
+-- dont fully understand all 3 groups
+group by 
     s.student_id,
-    s.student_name,
-    sbj.subject_name
+    sub.subject_name,
+    s.student_name
 
-order by s.student_id, sbj.subject_name;
-
-# time each student attended each exam
-# student_id, subject_name
-
-# cross join to ensure all students have every subject, even if they didnt attend class
+order by 
+    s.student_id, sub.subject_name;
