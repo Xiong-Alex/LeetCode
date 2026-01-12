02e07ae8e095ea1f3c92b309079f1db5a804618c
Xiong-Alex <102581551+Xiong-Alex@users.noreply.github.com>
2026-01-11 20:58:00 -0500

    Time: 326 ms (52.04%) | Memory: 0B (100.00%) - LeetSync

--- 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -1,10 +1,12 @@
 select e.employee_id 
 from Employees e
-where salary < 30000
-    and manager_id is not null
-    and not exists (
-        select 1
-        from Employees m
-        where m.employee_id = e.manager_id
+where salary < 30000                 -- check salary
+    and manager_id is not null       -- check if employee | managers have no manager_id
+    and not exists (                 -- check if employee / manager relationship exists
+
+        select 1  -- placeholder value; EXISTS only checks whether a row exists (true/false)
+        from Employees m 
+        where m.employee_id = e.manager_id -- chck if this manager exists by checking id
     )
+
 order by e.employee_id
