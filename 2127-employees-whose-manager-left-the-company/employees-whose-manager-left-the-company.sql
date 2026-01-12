15d8453f7a8efda67600a389470c6b3edf162c18
Xiong-Alex <102581551+Xiong-Alex@users.noreply.github.com>
2026-01-11 20:51:34 -0500

    Time: 328 ms (48.91%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -0,0 +1,10 @@
+select e.employee_id 
+from Employees e
+where salary < 30000
+    and manager_id is not null
+    and not exists (
+        select 1
+        from Employees m
+        where m.employee_id = e.manager_id
+    )
+order by e.employee_id
