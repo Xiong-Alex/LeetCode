0ffe2d6b8961a8120ce08ad5e3ace38bc0a684fe
Xiong-Alex <102581551+Xiong-Alex@users.noreply.github.com>
2026-02-07 21:17:22 -0500

    Time: 11 ms (83.82%) | Memory: 22.1 MB (45.17%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,17 +1,44 @@
+from collections import defaultdict
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 
-        anagram_keys = {}
+        anagram_keys = defaultdict(list)
 
         for word in strs:
 
             #destructure/breakdown word & rearange to create key
             key = ''.join(sorted(word))
 
-            if key not in anagram_keys:
-                anagram_keys[key] = []
-
+            #append to corresponding key
+            #default dict bypasses logic for checking if key alr exists
             anagram_keys[key].append(word)
 
         
         return list(anagram_keys.values())
+
+
+
+
+
+        
+
+# class Solution:
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+#         anagram_keys = {}
+
+#         for word in strs:
+
+#             #destructure/breakdown word & rearange to create key
+#             key = ''.join(sorted(word))
+
+#             if key not in anagram_keys:
+#                 anagram_keys[key] = []
+
+#             anagram_keys[key].append(word)
+
+        
+#         return list(anagram_keys.values())
+
+        
