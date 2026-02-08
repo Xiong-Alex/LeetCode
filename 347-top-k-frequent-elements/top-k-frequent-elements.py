2a78bfe23a095e2f1feeff32195db6353cb2b97c
Xiong-Alex <102581551+Xiong-Alex@users.noreply.github.com>
2026-02-07 21:47:52 -0500

    Time: 7 ms (54.19%) | Memory: 22.9 MB (51.98%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        # return dict | {number: count}
+        freq = Counter(nums)
+
+        #create list / sort via appearance / set reverrse / slice for first k elements
+        return sorted(freq, key=freq.get, reverse=True)[:k]
+
